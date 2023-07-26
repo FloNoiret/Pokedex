78fb6d84e65c88116bd2b0f61266be84e7ce9755
Flora Noiret <flo.n@hotmail.fr>
2023-07-26 17:14:14 +0200

    Creation of the Model Pokemon

---
+++ Pokemon.php
@@ -0,0 +1,145 @@
+<?php
+
+class Pokemon
+{
+    private $id;
+    private $number;
+    private $name;
+    private $description;
+    private $type1;
+    private $type2;
+
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data): void
+    {
+        foreach ($data as $key => $value) {
+            $method = "set" . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of number
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set the value of number
+     *
+     * @return  self
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type1
+     */
+    public function getType1()
+    {
+        return $this->type1;
+    }
+
+    /**
+     * Set the value of type1
+     *
+     * @return  self
+     */
+    public function setType1($type1)
+    {
+        $this->type1 = $type1;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type2
+     */
+    public function getType2()
+    {
+        return $this->type2;
+    }
+
+    /**
+     * Set the value of type2
+     *
+     * @return  self
+     */
+    public function setType2($type2)
+    {
+        $this->type2 = $type2;
+
+        return $this;
+    }
+}
